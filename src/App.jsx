2bc8eb7fab3c5a129095f1fb1eb90a3c7b606b62
vickne <30363517+vickne@users.noreply.github.com>
2024-09-24 18:59:26 +0300

    update new web

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Test from "./Test";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    {/*<section>
+      <Navbar/>
+    </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Contact</section>*/}
+    <section><Sidebar/></section>
+  </div>;
 };
 
 export default App;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="bg"> Links</div>
+    </div>
+  )
+}
+
+export default Sidebar
