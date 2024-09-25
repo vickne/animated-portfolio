7afd92f086425a50e1967db5bb6663b1a5accb86
vickne <30363517+vickne@users.noreply.github.com>
2024-09-25 19:11:29 +0300

    nn

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,14 @@
+import Links from "./links/Links"
 import "./sidebar.scss"
+import ToggleButton from "./toggleButton/ToggleButton"
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="bg"> Links</div>
+      <div className="bg"> 
+        <Links/>
+       </div>
+       <ToggleButton/>
     </div>
   )
 }
